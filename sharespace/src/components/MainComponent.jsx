4f0fcfd65f61b9968b1ae691b46275f5b11d6dc8
Sukhpreetsingh7137 <dk9592630@gmail.com>
2024-02-07 19:51:59 +0530

    changes done

--- sharespace/src/components/MainComponent.jsx
+++ sharespace/src/components/MainComponent.jsx
@@ -1,8 +1,18 @@
 const MainComponent=()=>{
-    return (<div className="bg-red-700">
-        <p>
-            Main Component
-        </p>
+    return (<div className="bg-red-700 py-10">
+        <div className="flex w-3/4 mx-auto gap-x-10">
+            <div className="bg-blue rounded-3xl p-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptates, quos dolorum impedit saepe adipisci, esse neque temporibus corporis tempora perspiciatis aperiam, officiis doloribus veritatis reiciendis? Reprehenderit impedit culpa iusto.
+            </div>
+            <div className="flex flex-col gap-y-5">
+                <div className="bg-primary rounded-3xl p-2">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam aperiam molestiae similique totam, quasi eos, eius dignissimos harum consequuntur ex esse non odio labore, exercitationem unde quis itaque perferendis? Porro!
+                </div>
+                <div className="bg-secondary rounded-3xl p-2">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem incidunt magni quia. Autem temporibus nobis, porro corrupti officiis repellat animi sequi excepturi consequuntur doloribus quos eveniet sit obcaecati debitis rem.
+                </div>
+            </div>
+        </div>
     </div>)
 }
 
