fdf02bea27996998b88e28c3b0519a85a84c030b
Shivansh Singh <77519009+shivanshsinghx365@users.noreply.github.com>
2024-03-02 06:52:30 +0530

    Refactor Header component and MainComponent styles

--- sharespace/src/components/Header.jsx
+++ sharespace/src/components/Header.jsx
@@ -3,50 +3,50 @@ import logo from "../assets/logo.png"
 import { useEffect } from "react";
 
 
-    
+
 
 const Header = () => {
-    const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        const currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-          document.getElementById("hide-header").style.top = "0";
-        } else {
-          document.getElementById("hide-header").style.top = "-80px"; // adjust this value to the height of your header
-        }
-        setPrevScrollpos(currentScrollPos);
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, [prevScrollpos]);
+  const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
 
-    return (
-        <div id="hide-header" style={{ position: 'fixed', width: '100%', zIndex: '100' }} className={`text-[0.95rem] p-2 border-b-2 border-[#131319] font-sans duration-75 backdrop-blur-sm bg-greenhero`}>
-            <div className="flex justify-between ml-10 mr-2">
-                <div className="basis-1/6 flex gap-x-2 my-auto mr-2">
-                    <img src={logo} alt="logo" className="w-9 h-9 my-auto invert-[100%]" />
-                    <div className="my-auto font-mono text-[1.123rem] mr-1">ShareSpace</div>
-                </div>
-                <div className="flex w-full my-auto">
-                    <div className="flex gap-x-10 text-[#d6d6d6]">
-                        <p className="cursor-pointer hover:text-slater">Pricing</p>
-                        <p className="cursor-pointer hover:text-slater">Features</p>
-                        <p className="cursor-pointer hover:text-slater">Vision</p>
-                        <p className="cursor-pointer hover:text-slater">FAQs</p>
-                    </div>
-                </div>
-                <div className="flex gap-x-[0.9rem] my-auto basis-[20%] min-w-[234px]">
-                    <p className="cursor-pointer text-red p-[0.4rem] w-full text-center rounded-md border-slate text-[#d6d6d6] border basis-2/3">Try for free</p>
-                    <p className="text-[#d6d6d6] bg-blue p-[0.4rem] w-full text-center rounded-md basis-1/3">Sign in</p>
-                </div>
-            </div>
-        </div>)
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("hide-header").style.top = "0px";
+      } else {
+        document.getElementById("hide-header").style.top = "-80px";
+      }
+      setPrevScrollpos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollpos]);
+
+  return (
+    <div id="hide-header" style={{ position: 'fixed', width: '100%', zIndex: '100' }} className={`text-[0.95rem] p-2 border-b-2 border-[#131319] font-sans backdrop-blur-sm bg-greenhero duration-150`}>
+      <div className="flex justify-between ml-10 mr-2">
+        <div className="basis-1/6 flex gap-x-2 my-auto mr-2">
+          <img src={logo} alt="logo" className="w-9 h-9 my-auto invert-[100%]" />
+          <div className="my-auto font-mono text-[1.123rem] mr-1">ShareSpace</div>
+        </div>
+        <div className="flex w-full my-auto">
+          <div className="flex gap-x-10 text-[#d6d6d6]">
+            <p className="cursor-pointer hover:text-slater">Pricing</p>
+            <p className="cursor-pointer hover:text-slater">Features</p>
+            <p className="cursor-pointer hover:text-slater">Vision</p>
+            <p className="cursor-pointer hover:text-slater">FAQs</p>
+          </div>
+        </div>
+        <div className="flex gap-x-[0.9rem] my-auto basis-[20%] min-w-[234px]">
+          <p className="cursor-pointer text-red p-[0.4rem] w-full text-center rounded-md border-slate text-[#d6d6d6] border basis-2/3">Try for free</p>
+          <p className="text-[#d6d6d6] bg-blue p-[0.4rem] w-full text-center rounded-md basis-1/3">Sign in</p>
+        </div>
+      </div>
+    </div>)
 }
 
 export default Header;
--- sharespace/src/components/MainComponent.jsx
+++ sharespace/src/components/MainComponent.jsx
@@ -1,5 +1,5 @@
 const MainComponent = () => {
-    return (<div className="bg-[#000000] text-[#d6d6d6] h-screen">
+    return (<div className="bg-[#000000] text-[#d6d6d6] h-screen duration-1000">
         hello
     </div>)
 }
